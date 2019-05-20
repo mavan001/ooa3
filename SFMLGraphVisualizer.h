835a3ff509aa5faa7d1c991b12ee3c30b78d6b29
mavan001 <49408205+mavan001@users.noreply.github.com>
2019-05-20 18:50:22 +0200

    Update SFMLGraphVisualizer.h

--- SFMLGraphVisualizer.h
+++ SFMLGraphVisualizer.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <math.h>
@@ -11,130 +12,130 @@ using namespace std;
 
 class SFMLGraphVisualizer {
 private:
-    sf::RenderWindow window;
-    sf::Font font;
+	sf::RenderWindow window;
+	sf::Font font;
 
 public:
 
-    SFMLGraphVisualizer() {
-        //load my font
-        this->font.loadFromFile("arial.ttf");
-    }
+	SFMLGraphVisualizer() {
+		//load my font
+		this->font.loadFromFile("arial.ttf");
+	}
 
-    void visualize(DiGraph &g) {
+	void visualize(DiGraph &g) {
 
-        window.create(sf::VideoMode(1024, 768), "myGraph");
+		window.create(sf::VideoMode(1024, 768), "myGraph");
 
-        Liste<Node > nodes = g.getNodes();
+		Liste<Node *> nodes = g.getNodes();
 
-        while (window.isOpen()) {
-            // Process events
-            sf::Event event;
+		while (window.isOpen()) {
+			// Process events
+			sf::Event event;
 
-            while (window.pollEvent(event)) {
-                // Close window: exit
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
+			while (window.pollEvent(event)) {
+				// Close window: exit
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
 
-            // Clear screen
-            window.clear(sf::Color::White);
-            for (unsigned int i = 0; i < nodes.getListSize(); i++) {
-                Node *node = nodes.getValueAtPos(i);
+			// Clear screen
+			window.clear(sf::Color::White);
+			for (unsigned int i = 0; i < nodes.getListSize(); i++) {
+				Node *node = nodes.getValueAtPos(i);
 
-                sf::Color color(255, 0, 0);
-                drawNode(*node, color);
+				sf::Color color(255, 0, 0);
+				drawNode(*node, color);
 
-                // Ausgehende Kanten einzeichnen
-                Liste<Edge > edges = g.getEdges(node->getKey());
+				// Ausgehende Kanten einzeichnen
+				Liste<Edge *> edges = g.getEdges(node->getKey());
 
-                for (unsigned int j = 0; j < edges.getListSize(); j++) {
-                    drawEdge(*(edges.getValueAtPos(j)), sf::Color::Black,
-                             edges.getValueAtPos(j)->getWeight(), 1);
-                }
-            }
-            // Update the window
-            window.display();
-        }
-    }
-
-
-    void drawNode(Node &node, sf::Color nodeColor) {
-
-        sf::CircleShape Circle(NODERADIUS);
-        Circle.setPosition(node.getPositionX() - NODERADIUS,
-                           node.getPositionY() - NODERADIUS);
-        Circle.setFillColor(sf::Color::White);
-        Circle.setOutlineColor(nodeColor);
-        Circle.setOutlineThickness(5);
-
-        window.draw(Circle);
-
-        sf::Text Label(node.getKey(), font, 32);
-        Label.setPosition(node.getPositionX() - NODERADIUS / 2 + 5,
-                          node.getPositionY() - NODERADIUS / 2 - 5);
-        Label.setFillColor(sf::Color::Blue);
-
-        window.draw(Label);
-    }
+				for (unsigned int j = 0; j < edges.getListSize(); j++) {
+					drawEdge(*(edges.getValueAtPos(j)), sf::Color::Black,
+						edges.getValueAtPos(j)->getWeight(), 1);
+				}
+			}
+			// Update the window
+			window.display();
+		}
+	}
 
 
-    void drawEdge(Edge &edge, sf::Color color, double weight,
-                  int thickness = 1, int arrowMagnitude = 20) {
+	void drawNode(Node &node, sf::Color nodeColor) {
 
-        sf::Vector2f p(edge.getStartNode()->getPositionX(),
-                       edge.getStartNode()->getPositionY());
-        sf::Vector2f q(edge.getEndNode()->getPositionX(),
-                       edge.getEndNode()->getPositionY());
+		sf::CircleShape Circle(NODERADIUS);
+		Circle.setPosition(node.getPositionX() - NODERADIUS,
+			node.getPositionY() - NODERADIUS);
+		Circle.setFillColor(sf::Color::White);
+		Circle.setOutlineColor(nodeColor);
+		Circle.setOutlineThickness(5);
 
+		window.draw(Circle);
 
-        // Berechne den Winkel
-        const double PI = 3.141592653;
-        double angle = atan2((double) p.y - q.y, (double) p.x - q.x);
+		sf::Text Label(node.getKey(), font, 32);
+		Label.setPosition(node.getPositionX() - NODERADIUS / 2 + 5,
+			node.getPositionY() - NODERADIUS / 2 - 5);
+		Label.setFillColor(sf::Color::Blue);
 
-        // Berechne verkuerzten Pfeil
-        q.x = (int) (q.x + NODERADIUS * cos(angle));
-        q.y = (int) (q.y + NODERADIUS * sin(angle));
-        p.x = (int) (p.x - NODERADIUS * cos(angle));
-        p.y = (int) (p.y - NODERADIUS * sin(angle));
-
-        sf::Vertex line[2] =
-                {
-                        sf::Vertex(sf::Vector2f(p.x, p.y), color),
-                        sf::Vertex(sf::Vector2f(q.x, q.y), color)
-                };
-
-        // thickness
-        window.draw(line, 2, sf::Lines);
-
-        std::stringstream weightstring;
-        weightstring << weight;
-        sf::Text Labelweight(weightstring.str(), font, 32);
-        int size = sqrt(pow(p.x - q.x, 2) + pow(p.y - q.y, 2));
-        Labelweight.setPosition(p.x - (size / 2)*cos(angle)+10*sin(angle),
-                                p.y - (size / 2)*sin(angle)+10*cos(angle));
-        Labelweight.setFillColor(sf::Color::Blue);
-        window.draw(Labelweight);
-
-        //Erstes Segment
-        p.x = (int) (q.x + arrowMagnitude * cos(angle + PI / 8));
-        p.y = (int) (q.y + arrowMagnitude * sin(angle + PI / 8));
-        sf::Vertex first[2] =
-                {
-                        sf::Vertex(sf::Vector2f(p.x, p.y), color),
-                        sf::Vertex(sf::Vector2f(q.x, q.y), color)
-                };
-        window.draw(first, 2, sf::Lines);
+		window.draw(Label);
+	}
 
 
-        //Zweites Segment
-        p.x = (int) (q.x + arrowMagnitude * cos(angle - PI / 8));
-        p.y = (int) (q.y + arrowMagnitude * sin(angle - PI / 8));
-        sf::Vertex second[2] =
-                {
-                        sf::Vertex(sf::Vector2f(p.x, p.y), color),
-                        sf::Vertex(sf::Vector2f(q.x, q.y), color)
-                };
-        window.draw(second, 2, sf::Lines);
-    }
+	void drawEdge(Edge &edge, sf::Color color, double weight,
+		int thickness = 1, int arrowMagnitude = 20) {
+
+		sf::Vector2f p(edge.getStartNode()->getPositionX(),
+			edge.getStartNode()->getPositionY());
+		sf::Vector2f q(edge.getEndNode()->getPositionX(),
+			edge.getEndNode()->getPositionY());
+
+
+		// Berechne den Winkel
+		const double PI = 3.141592653;
+		double angle = atan2((double)p.y - q.y, (double)p.x - q.x);
+
+		// Berechne verkuerzten Pfeil
+		q.x = (int)(q.x + NODERADIUS * cos(angle));
+		q.y = (int)(q.y + NODERADIUS * sin(angle));
+		p.x = (int)(p.x - NODERADIUS * cos(angle));
+		p.y = (int)(p.y - NODERADIUS * sin(angle));
+
+		sf::Vertex line[2] =
+		{
+				sf::Vertex(sf::Vector2f(p.x, p.y), color),
+				sf::Vertex(sf::Vector2f(q.x, q.y), color)
+		};
+
+		// thickness
+		window.draw(line, 2, sf::Lines);
+
+		std::stringstream weightstring;
+		weightstring << weight;
+		sf::Text Labelweight(weightstring.str(), font, 32);
+		int size = sqrt(pow(p.x - q.x, 2) + pow(p.y - q.y, 2));
+		Labelweight.setPosition(p.x - (size / 2)*cos(angle) + 10 * sin(angle),
+			p.y - (size / 2)*sin(angle) + 10 * cos(angle));
+		Labelweight.setFillColor(sf::Color::Blue);
+		window.draw(Labelweight);
+
+		//Erstes Segment
+		p.x = (int)(q.x + arrowMagnitude * cos(angle + PI / 8));
+		p.y = (int)(q.y + arrowMagnitude * sin(angle + PI / 8));
+		sf::Vertex first[2] =
+		{
+				sf::Vertex(sf::Vector2f(p.x, p.y), color),
+				sf::Vertex(sf::Vector2f(q.x, q.y), color)
+		};
+		window.draw(first, 2, sf::Lines);
+
+
+		//Zweites Segment
+		p.x = (int)(q.x + arrowMagnitude * cos(angle - PI / 8));
+		p.y = (int)(q.y + arrowMagnitude * sin(angle - PI / 8));
+		sf::Vertex second[2] =
+		{
+				sf::Vertex(sf::Vector2f(p.x, p.y), color),
+				sf::Vertex(sf::Vector2f(q.x, q.y), color)
+		};
+		window.draw(second, 2, sf::Lines);
+	}
 };
