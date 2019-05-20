be9e4a0d01073361feaa8e32b4d754039c35ee9f
mavan001 <49408205+mavan001@users.noreply.github.com>
2019-05-20 18:51:37 +0200

    Update DiGraph.cpp

--- DiGraph.cpp
+++ DiGraph.cpp
@@ -1,52 +1,55 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 #include "Liste.h"
 #include "Node.h"
 #include "DiGraph.h"
+#include "pqueue.h"
 
 
 DiGraph::DiGraph() {
-    _nodeList = Liste<Node>();
+	_nodeList = new Liste<Node*>();
 }
 
 void DiGraph::addNode(Node * node) {
-    _nodeList.insertNewEntry(node);
+	_nodeList->insertNewEntry(node);
 }
 void DiGraph::addEdge(string key1, string key2, float weight) {
-    Node *startNode = getNodeAdress(key1);
-    Node *endNode = getNodeAdress(key2);
-    if (startNode != endNode)
-        startNode->setNewEdge(new Edge(weight, startNode, endNode));
+	Node *startNode = getNodeAdress(key1);
+	Node *endNode = getNodeAdress(key2);
+	if (startNode != endNode)
+		startNode->setNewEdge(new Edge(weight, startNode, endNode));
 }
 
-Liste<Node> DiGraph::getNeighbours(string key) {
-    Liste<Node> resultList;
-    Liste<Edge> neighboursList;
-    neighboursList = getNodeAdress(key)->getEdges();
+Liste<Node *> DiGraph::getNeighbours(string key) {
+	Liste<Node *> resultList;
+	Liste<Edge *> neighboursList;
+	neighboursList = getNodeAdress(key)->getEdges();
 
-    for (int i = 0; i < neighboursList.getListSize(); i++) {
-        resultList.insertNewEntry(neighboursList.getValueAtPos(i)->getEndNode());
-    }
+	for (int i = 0; i < neighboursList.getListSize(); i++) {
+		resultList.insertNewEntry(neighboursList.getValueAtPos(i)->getEndNode());
+	}
 
-    return resultList;
+	return resultList;
 }
 
 
-Liste <Edge> DiGraph::getEdges(string key) {
-    for (int i = 0; i < _nodeList.getListSize() ; i++) {
-        if (key == _nodeList.getValueAtPos(i)->getKey()) {
-            return _nodeList.getValueAtPos(i)->getEdges();
-        }
-    }
+Liste <Edge *> DiGraph::getEdges(string key) {
+	for (int i = 0; i < _nodeList->getListSize(); i++) {
+		if (key == _nodeList->getValueAtPos(i)->getKey()) {
+			return _nodeList->getValueAtPos(i)->getEdges();
+		}
+	}
 }
 
-Liste <Node> DiGraph::getNodes(void) {
-    return _nodeList;
+Liste <Node *> DiGraph::getNodes(void) {
+	return * _nodeList;
 }
 
 Node* DiGraph::getNodeAdress(string key) {
-    for (int i = 0; i < _nodeList.getListSize(); i++) {
-        if (key == _nodeList.getValueAtPos(i)->getKey()) {
-            return _nodeList.getValueAtPos(i);
-        }
-    }
+	for (int i = 0; i < _nodeList->getListSize(); i++) {
+		if (key == _nodeList->getValueAtPos(i)->getKey()) {
+			return _nodeList->getValueAtPos(i);
+		}
+	}
 }
