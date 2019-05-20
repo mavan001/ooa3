0d78409266c2c9122d16c476eff676ba8c759e6b
mavan001 <49408205+mavan001@users.noreply.github.com>
2019-05-20 18:50:51 +0200

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,64 +1,64 @@
 #include "Node.h"
 
 Node::Node(string key, int x, int y) {
-    _key = key;
-    _posX = x;
-    _posY = y;
-    _edgeList = new Liste<Edge>();
+	_key = key;
+	_posX = x;
+	_posY = y;
+	_edgeList = new Liste<Edge *>();
 }
 
-string Node::getKey(void){
-    return _key;
+string Node::getKey(void) {
+	return _key;
 }
 int Node::getPositionX(void) {
-    return _posX;
+	return _posX;
 }
 
 int Node::getPositionY(void) {
-    return _posY;
+	return _posY;
 }
 
-Liste<Edge> Node::getEdges(void) {
-    return * _edgeList;
+Liste<Edge *> Node::getEdges(void) {
+	return *_edgeList;
 }
 
 void Node::setKey(string newKey) {
-    _key = newKey;
+	_key = newKey;
 }
 void Node::setPositionX(int x) {
-    _posX = x;
+	_posX = x;
 }
 void Node::setPositionY(int y) {
-    _posY = y;
+	_posY = y;
 }
 void Node::setNewEdge(Edge * edge) {
-    _edgeList->insertNewEntry(edge);
+	_edgeList->insertNewEntry(edge);
 }
 
 Edge::Edge(float weight, Node *startNode, Node *endNode) {
-    _weight = weight;
-    _startNode = startNode;
-    _endNode = endNode;
+	_weight = weight;
+	_startNode = startNode;
+	_endNode = endNode;
 }
 
 float Edge::getWeight(void) {
-    return _weight;
+	return _weight;
 }
 
 Node *Edge::getStartNode(void) {
-    return _startNode;
+	return _startNode;
 }
 
 Node *Edge::getEndNode(void) {
-    return _endNode;
+	return _endNode;
 }
 
 void Edge::setWeight(float w) {
-    _weight = w;
+	_weight = w;
 }
 void Edge::setStartNode(Node * n) {
-    _startNode = n;
+	_startNode = n;
 }
 void Edge::setEndNode(Node * n) {
-    _endNode = n;
+	_endNode = n;
 }
